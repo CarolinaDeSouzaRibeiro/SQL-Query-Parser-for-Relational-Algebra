977e29c641b9c8660f44de098b0c1c691c16a5dc
C-dS-R <carolinadesouzaribeiro@hotmail.com>
2025-04-15 09:03:34 -0300

    push inicial

---
+++ parser.py
@@ -0,0 +1,20 @@
+#parse SQL based instructions
+#i. Select, From, Where, INNER JOIN;
+#ii. Operators =, >, <, <=, >=, <>, And, ( , ) ;
+
+#all commands must begin with SELECT
+#elements must be separated by commas
+
+#FROM -> must be followed by an existing table name
+#WHERE -> must be followed by a valid condition
+#INNER JOIN -> must be followed by an existing table name
+
+#operators:
+# = has to be between two elements, either a column name or a value
+# =, >, <, <=, >=, <>, do the same
+#AND -> must be between two valid conditions
+#( -> must be followed by a valid operation, column name or condition
+#) -> must be preceded by a valid condition and close a parenthesis
+
+#
+
