316ff70ae339ebc309401b457dd482c1b8224d3e
C-dS-R <carolinadesouzaribeiro@hotmail.com>
2025-04-19 18:39:25 -0300

    UI inicial

---
+++ UI/main.py
@@ -0,0 +1,25 @@
+import gradio as gr
+
+def test(comando):
+    return f"Seu comando: {comando}",f"Seu comando: {comando}", '<div id="grafo"></div>','<div id="grafo"></div>'
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("## Ativ algebra relacional")
+    with gr.Row():
+        with gr.Column():
+            cmd_sql = gr.Textbox(label="Comando SQL")
+            btn = gr.Button("Submit")
+        with gr.Column():
+            gr.Markdown("## Algebra Relacional")
+            algeb_relac_otim = gr.Textbox(label="Algebra Relacional (Otimizado)")
+            algeb_relac = gr.Textbox(label="Algebra Relacional (Não otimizado)")
+
+            gr.Markdown("## Grafos")
+            grafo_otim = gr.HTML('')
+            grafo = gr.HTML('')
+
+        #comando do botao
+        btn.click(test, inputs=[cmd_sql], outputs=[algeb_relac_otim, algeb_relac, grafo_otim, grafo])
+
+demo.launch()
