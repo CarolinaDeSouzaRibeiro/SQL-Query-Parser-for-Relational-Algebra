3024d731f6e50c89b38e8fdc2b851543f983e1c0
C-dS-R <carolinadesouzaribeiro@hotmail.com>
2025-04-22 07:30:41 -0300

    Update main.py

--- UI/main.py
+++ UI/main.py
@@ -1,8 +1,12 @@
 import gradio as gr
 
 def test(comando):
-    return f"Seu comando: {comando}",f"Seu comando: {comando}", '<div id="grafo"></div>','<div id="grafo"></div>'
+    comando_ordenado = comando
+    grafo_otimizado = '<div id="grafo">A</div>'
+    grafo = '<div id="grafo">B</div>'
+    ordem_execucao = '<div id="ordem"><ol><li>aaa</li><li>bbb</li></ol></div>'
 
+    return f"Seu comando: {comando}", comando_ordenado, grafo_otimizado, grafo, ordem_execucao
 
 with gr.Blocks() as demo:
     gr.Markdown("## Processador de consultas")
@@ -16,12 +20,13 @@ with gr.Blocks() as demo:
             algeb_relac = gr.Textbox(label="Algebra Relacional (Não otimizado)")
 
             gr.Markdown("## Grafos")
-            grafo_otim = gr.HTML('')
-            grafo = gr.HTML('')
+            grafo_otim = gr.HTML()
+            grafo = gr.HTML()
 
             gr.Markdown("## Ordem de execução")
+            ordem_exec = gr.HTML()
 
         #comando do botao
-        btn.click(test, inputs=[cmd_sql], outputs=[algeb_relac_otim, algeb_relac, grafo_otim, grafo])
+        btn.click(test, inputs=[cmd_sql], outputs=[algeb_relac_otim, algeb_relac, grafo_otim, grafo, ordem_exec])
 
 demo.launch()
